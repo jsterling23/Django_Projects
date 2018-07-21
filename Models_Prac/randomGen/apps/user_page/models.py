733d3f045b466a1da7fd8cabb91ac8d233c1dfc4
Jerry Padilla <jerrypadilla23@gmail.com>
2018-07-21 00:24:31 -0500

    Falling asleep. Tackle profile model update/create

--- Models_Prac/randomGen/apps/user_page/models.py
+++ Models_Prac/randomGen/apps/user_page/models.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import birthday
 from django.db import models
-
+from ..logReg.models import User
 # Create your models here.
 
+
 class Bio(models.Model):
-    profile_bio = TextField()
-    profile_birthday = birthday.fields.BirthdayField()
-    profile_home_town = 
-    profile_hobbies = 
-    profile_favorite_genre = 
-    user = 
-    created_at = 
-    updated_at = 
+    profile_bio = models.TextField(blank=True)
+    profile_birthday = models.DateTimeField(blank=True)
+    profile_home_town = models.CharField(max_length=255,blank=True)
+    profile_hobbies = models.TextField(blank=True)
+    profile_favorite_genre = models.CharField(max_length=20, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return('Bio: {} || Birthday: {} || Home Town: {} || Hobbies: {} || Fav Music Genre: {} || User: {} ').format(self.profile_bio, self.profile_birthday, self.profile_home_town, self.profile_hobbies, self.profile_favorite_genre, self.user)
--- Models_Prac/randomGen/apps/user_page/views.py
+++ Models_Prac/randomGen/apps/user_page/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from ..logReg.models import User
 from ..home.models import Thought
+from ..user_page.models import Bio
 
 # Create your views here.
 
@@ -16,8 +17,11 @@ def index(req):
 
         user = User.objects.get(id=req.session['user_id'])
         thoughts = Thought.objects.filter(user=user)
+        bio = Bio.objects.filter(user=user)
+
         context = {
             'user_thoughts':thoughts,
+            'bio':bio,
         }
     return render(req, template, context)
 
@@ -35,4 +39,36 @@ def edit_bio(req):
 
 def process_edit_bio(req):
     
+    bio = req.POST['profile_bio']
+    birthday = req.POST['profile_birthday']
+    home_town = req.POST['profile_home_town']
+    hobbies = req.POST['profile_hobbies']
+    genre = req.POST['profile_favorite_genre']
+    user = User.objects.get(id=req.session['user_id'])
+
+    if Bio.objects.filter(user=user).exists():
+        print('where i need tobe')
+        bio_update = Bio.objects.update(
+            profile_bio = bio,
+            profile_birthday = birthday,
+            profile_home_town = home_town,
+            profile_hobbies = hobbies,
+            profile_favorite_genre = genre,
+            user = user,
+        )
+        bio_update.save()
+
+    else:
+        print('not where i need to be')
+        Bio.objects.create(
+            profile_bio = bio,
+            profile_birthday = birthday,
+            profile_home_town = home_town,
+            profile_hobbies = hobbies,
+            profile_favorite_genre = genre,
+            user = user,
+        )
+
+    print(bio, birthday, home_town, hobbies, genre, user)
+
     return redirect('user_page:index')
